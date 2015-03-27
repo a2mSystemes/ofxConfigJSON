df91a779a313c144283e69c13f7fbc2084237669
a2mSystemes <a-m.maurin@a2msystemes.fr>
2015-03-27 17:14:00 +0100

    first release

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+meta:
+	ADDON_NAME = ofxConfigJSON
+	ADDON_DESCRIPTION = load and read config from and to a json formated file
+	ADDON_AUTHOR = A2M Systemes
+	ADDON_TAGS = "json" "web" "config" "utilities"
+	ADDON_URL = http://github.com/bakercp/ofxHTTP
+common:
+	# dependencies with other addons, a list of them separated by spaces 
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxSSLManager ofxIO ofxMediaType ofxTaskQueue ofxHTTP ofxJSON
